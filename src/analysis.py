f3d6005459d97105fce2f2c15c3e822bee4f6061
Linus Marco <lmarco163@gmail.com>
2017-03-03 20:30:30 -0500

    basic position-agnostic z-scores

--- src/analysis.py
+++ src/analysis.py
@@ -11,19 +11,35 @@ ROSTER_SIZE_P = 9
 CATEGORIES_H = ['OBP', 'SLG', 'HR', 'R', 'SB']
 
 
+def calc_zscores(col):
+    mean = col.mean()
+    std = col.std(ddof=0)
+    z = (col - mean)/std
+    return z
+
+
 def main():
     hitters = pd.read_excel(os.path.join(DATA_DIR, PECOTA_FILE), sheetname="Hitters")
-    hitters = hitters[['FIRSTNAME', 'LASTNAME'] + CATEGORIES_H]
+    hitters = hitters[['FIRSTNAME', 'LASTNAME', 'POS'] + CATEGORIES_H]
     print('UNIVERSE: {}'.format(len(hitters)))
 
     hitters_top = pd.DataFrame()
     for cat in CATEGORIES_H:
-        hitters_top = hitters_top.append(hitters.nlargest(ROSTER_SIZE_H*TEAMS, cat))
+        hitters_top = hitters_top.append(hitters.nlargest(int(ROSTER_SIZE_H*TEAMS), cat))
 
     hitters_top.drop_duplicates(inplace=True)
-    print('TOP 200 ANY CATEGORY: {}'.format(len(hitters_top)))
+    print('TOP ANY CATEGORY: {}'.format(len(hitters_top)))
 
+    z_cols = [x + '_zscore' for x in CATEGORIES_H]
+    hitters_top[z_cols] = hitters_top[CATEGORIES_H].apply(calc_zscores)
+    hitters_top['all_zscore'] = hitters_top[z_cols].sum(axis=1)
+    hitters_top.sort_values(by='all_zscore', ascending=False, inplace=True)
+    hitters_top.reset_index(drop=True, inplace=True)
+    hitters_top['rank'] = hitters_top.index + 1
 
+    hitters_top.to_csv(os.path.join(DATA_DIR, "hitters_top.csv"))
+
+    print(hitters_top.head(50))
 
 
 if (__name__ == "__main__"):
