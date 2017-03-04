92caaa72488e1693f1b2d8d2d6ab3821970364f0
Linus Marco <lmarco163@gmail.com>
2017-03-04 15:30:34 -0500

    rework weighting to adjust for playing time

--- src/analysis.py
+++ src/analysis.py
@@ -1,6 +1,8 @@
 import os
 import numpy as np
 import pandas as pd
+import matplotlib
+import matplotlib.pyplot as plt
 
 
 DATA_DIR = "/Users/linusmarco/documents/data/PECOTA"
@@ -8,8 +10,20 @@ PECOTA_FILE = "pecota_2017_03_01_86394.xls"
 TEAMS = 9
 ROSTER_SIZE_H = 12
 ROSTER_SIZE_P = 9
-CATEGORIES_H = ['OBP', 'SLG', 'HR', 'R', 'SB']
-CATEGORIES_P = ['ERA', 'WHIP', 'QS', 'SO', 'SV']
+CATEGORIES_H = [
+    {'name': 'OBP', 'weight': 'PA' },
+    {'name': 'SLG', 'weight': 'AB' },
+    {'name': 'HR', 'weight': None },
+    {'name': 'R', 'weight': None },
+    {'name': 'SB', 'weight': None }
+]
+CATEGORIES_P = [
+    {'name': 'ERA', 'weight': 'IP' },
+    {'name': 'WHIP', 'weight': 'IP' },
+    {'name': 'QS', 'weight': None },
+    {'name': 'SO', 'weight': None },
+    {'name': 'SV', 'weight': None }
+]
 
 
 def calc_zscores(col):
@@ -19,36 +33,65 @@ def calc_zscores(col):
     return z
 
 
-def rank(players, categories, rosternum, wt_cats=None, wt_col=None, bypos=False):
+def weight_column(col, wt_col):
+    mean = col.mean()
+    weighted = (col - mean)*wt_col
+    return weighted
+
+
+def rank(players, categories, rosternum, bypos=False):
+    df = players.copy()
+
+    id_cols = ['FIRSTNAME', 'LASTNAME']
     if bypos:
-        id_cols = ['FIRSTNAME', 'LASTNAME', 'POS']
-    else:
-        id_cols = ['FIRSTNAME', 'LASTNAME']
+        id_cols.append('POS')
 
-    if (wt_col != None):
-        id_cols.append(wt_col)
+    # set up and weight columns for analysis
+    raw_cols = []
+    val_cols = []
+    wt_cols = []
+    for cat in categories:
+        raw_cols.append(cat['name'])
+        if (cat['weight'] != None):
+            df[cat['name'] + '_WT'] = weight_column(df[cat['name']], df[cat['weight']])
+            val_cols.append(cat['name'] + '_WT')
+            if (cat['weight'] not in wt_cols):
+                wt_cols.append(cat['weight'])
+        else:
+            val_cols.append(cat['name'])
 
-    df = players.copy()[id_cols + categories]
-
+    # limit player universe
     top = pd.DataFrame()
     for cat in categories:
-        top = top.append(df.nlargest(rosternum, cat))
+        if (cat['weight'] == None):
+            largest = df.nlargest(rosternum, cat['name'])
+        else:
+            largest = df.nlargest(rosternum, cat['name'] + '_WT')
+        top = top.append(largest)
+
     top.drop_duplicates(inplace=True)
 
-    z_cols = [x + '_zscore' for x in categories]
+    # calculate z scores for each category
+    z_cols = [c + '_zscore' for c in raw_cols]
     if bypos:
-        top[z_cols] = top.groupby('POS')[categories].apply(calc_zscores)
+        top[z_cols] = top.groupby('POS')[val_cols].apply(calc_zscores)
     else:
-        top[z_cols] = top[categories].apply(calc_zscores)
+        top[z_cols] = top[val_cols].apply(calc_zscores)
 
-    if (wt_cats != None and wt_col != None):
-        wt_z_cols = [x + '_zscore' for x in wt_cats]
-        top[wt_z_cols] = top[wt_z_cols].apply(lambda x: x * top[wt_col])
-
+    # rank by sum of z scores
     top['all_zscore'] = top[z_cols].sum(axis=1)
     top.sort_values(by='all_zscore', ascending=False, inplace=True)
     top.reset_index(drop=True, inplace=True)
     top['rank'] = top.index + 1
+
+    # organize columns
+    cols = ['rank', 'all_zscore']
+    cols = cols + id_cols + wt_cols
+    for c in categories:
+        cols.append(c['name'])
+        cols.append(c['name'] + '_zscore')
+
+    top = top[cols]
 
     print(top.head(50))
     return top
@@ -57,14 +100,14 @@ def rank(players, categories, rosternum, wt_cats=None, wt_col=None, bypos=False)
 def main():
     hitters = pd.read_excel(os.path.join(DATA_DIR, PECOTA_FILE), sheetname="Hitters")
     hitters['POS'].replace(to_replace=['LF', 'CF', 'RF'], value='OF', inplace=True)
-    hitters_top = rank(hitters, CATEGORIES_H, ROSTER_SIZE_H*TEAMS, wt_cats=['OBP', 'SLG'], wt_col='PA', bypos=True)
-    hitters_top.to_csv(os.path.join(DATA_DIR, "hitters.csv"))
+    hitters_top = rank(hitters, CATEGORIES_H, ROSTER_SIZE_H*TEAMS, bypos=True)
+    hitters_top.to_csv(os.path.join(DATA_DIR, "hitters.csv"), index=False)
 
     pitchers = pd.read_excel(os.path.join(DATA_DIR, PECOTA_FILE), sheetname="Pitchers")
     pitchers[['ERA', 'WHIP']] = pitchers[['ERA', 'WHIP']].multiply(-1)
-    pitchers_top = rank(pitchers, CATEGORIES_P, ROSTER_SIZE_P*TEAMS, wt_cats=['ERA', 'WHIP'], wt_col='IP', bypos=False)
+    pitchers_top = rank(pitchers, CATEGORIES_P, ROSTER_SIZE_P*TEAMS, bypos=False)
     pitchers_top[['ERA', 'WHIP']] = pitchers_top[['ERA', 'WHIP']].multiply(-1)
-    pitchers_top.to_csv(os.path.join(DATA_DIR, "pitchers.csv"))
+    pitchers_top.to_csv(os.path.join(DATA_DIR, "pitchers.csv"), index=False)
 
 
 if (__name__ == "__main__"):
