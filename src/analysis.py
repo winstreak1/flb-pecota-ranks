a2a0838dd88061f24828cce16f7606482c856c58
Linus Marco <lmarco163@gmail.com>
2017-03-03 22:26:14 -0500

    added weights --still need to adjust to work with other z scores

--- src/analysis.py
+++ src/analysis.py
@@ -19,11 +19,15 @@ def calc_zscores(col):
     return z
 
 
-def rank(players, categories, rosternum, bypos=False):
+def rank(players, categories, rosternum, wt_cats=None, wt_col=None, bypos=False):
     if bypos:
         id_cols = ['FIRSTNAME', 'LASTNAME', 'POS']
     else:
         id_cols = ['FIRSTNAME', 'LASTNAME']
+
+    if (wt_col != None):
+        id_cols.append(wt_col)
+
     df = players.copy()[id_cols + categories]
 
     top = pd.DataFrame()
@@ -36,6 +40,11 @@ def rank(players, categories, rosternum, bypos=False):
         top[z_cols] = top.groupby('POS')[categories].apply(calc_zscores)
     else:
         top[z_cols] = top[categories].apply(calc_zscores)
+
+    if (wt_cats != None and wt_col != None):
+        wt_z_cols = [x + '_zscore' for x in wt_cats]
+        top[wt_z_cols] = top[wt_z_cols].apply(lambda x: x * top[wt_col])
+
     top['all_zscore'] = top[z_cols].sum(axis=1)
     top.sort_values(by='all_zscore', ascending=False, inplace=True)
     top.reset_index(drop=True, inplace=True)
@@ -46,14 +55,14 @@ def rank(players, categories, rosternum, bypos=False):
 
 
 def main():
-    hitters = pd.read_excel(os.path.join(DATA_DIR, PECOTA_FILE), sheetname="Hitters")
-    hitters['POS'].replace(to_replace=['LF', 'CF', 'RF'], value='OF', inplace=True)
-    hitters_top = rank(hitters, CATEGORIES_H, ROSTER_SIZE_H*TEAMS, bypos=True)
-    hitters_top.to_csv(os.path.join(DATA_DIR, "hitters.csv"))
+    # hitters = pd.read_excel(os.path.join(DATA_DIR, PECOTA_FILE), sheetname="Hitters")
+    # hitters['POS'].replace(to_replace=['LF', 'CF', 'RF'], value='OF', inplace=True)
+    # hitters_top = rank(hitters, CATEGORIES_H, ROSTER_SIZE_H*TEAMS, bypos=True)
+    # hitters_top.to_csv(os.path.join(DATA_DIR, "hitters.csv"))
 
     pitchers = pd.read_excel(os.path.join(DATA_DIR, PECOTA_FILE), sheetname="Pitchers")
     pitchers[['ERA', 'WHIP']] = pitchers[['ERA', 'WHIP']].multiply(-1)
-    pitchers_top = rank(pitchers, CATEGORIES_P, ROSTER_SIZE_P*TEAMS, bypos=False)
+    pitchers_top = rank(pitchers, CATEGORIES_P, ROSTER_SIZE_P*TEAMS, wt_cats=['ERA', 'WHIP'], wt_col='IP', bypos=False)
     pitchers_top[['ERA', 'WHIP']] = pitchers_top[['ERA', 'WHIP']].multiply(-1)
     pitchers_top.to_csv(os.path.join(DATA_DIR, "pitchers.csv"))
 
